a98222a7c8867471b6178381b3ced34dc799c7de
daniel moreno <daniel.moreno.reyes@gmail.com>
2022-07-11 11:27:49 -0500

    carga inicial

---
+++ 04_microservicio_contactos/src/main/java/dao/AgendaJpaSpring.java
@@ -0,0 +1,19 @@
+package dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import model.Contacto;
+
+public interface AgendaJpaSpring extends JpaRepository<Contacto, Integer>{
+ 
+	Contacto findByEmail(String email);
+		@Transactional
+		@Modifying
+		@Query("Delete from Contacto c where c.email=?1")
+		void eliminarPorEmail(String email);
+	
+	
+}
---
+++ micreoservicio_ejemplo/src/main/java/com/boot/ejemplo/MicreoservicioEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.boot.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicreoservicioEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicreoservicioEjemploApplication.class, args);
+	}
+
+}
